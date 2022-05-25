cea9cb9d16bc143747601669fd5d114586b37813
H0RSESH0E <94808313+H0RSESH0E@users.noreply.github.com>
2022-05-25 14:04:31 -0400

    modified basic visual components favicon etc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './pet-location.svg';
 import './App.css';
 
 function App() {
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          How about: Pet Alert as the name?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+ 
       </header>
     </div>
   );
